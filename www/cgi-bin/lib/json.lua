baffbc424efd49ab24d1a76c812a50e35ce849b9
suzukiin <lucas14112003@gmail.com>
2026-01-08 17:26:35 -0300

    path config.json

--- www/cgi-bin/lib/json.lua
+++ www/cgi-bin/lib/json.lua
@@ -1,14 +1,22 @@
 #!/usr/bin/lua
 local cjson = require "cjson"
-local f = io.open("/home/lucas/overlay/www/config.json", "r")
 
 local M = {}
 
 function M.getInfo()
+    local f, err = io.open("/www/config.json", "r")
+    if not f then
+        return nil, "erro ao abrir config.json: " .. (err or "desconhecido")
+    end
+
     local content = f:read("*a")
     f:close()
 
-    local data = cjson.decode(content)
+    local data, perr = cjson.decode(content)
+    if not data then
+        return nil, "erro ao decodificar JSON: " .. (perr or "desconhecido")
+    end
+
     return data.info
 end
 
